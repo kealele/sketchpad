c8018a625042e66f1c2d4e1ffd9cb6d31a1d17d9
kealele <whiski20@gmail.com>
2023-09-28 17:39:50 +0300

    Created resize button script

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('#grid');
+const sizebutton = document.querySelector(".size");
 
 function createGrid (x){
     for (let rows = 0; rows < x; rows++){
@@ -13,4 +14,27 @@ function createGrid (x){
     }
     }
 }
-createGrid (16);
+
+createGrid(16);
+
+function resizeGrid (){
+    grid.innerHTML = '';
+    let x = prompt("Enter the number of squares per side. Cancel button will revert grid back to default value - 16");
+    if (isNaN(x)){
+        resizeGrid();
+    } else if (x < 0){
+        alert("Please, use a positive value")
+        resizeGrid();
+    } else if (x === null){
+        createGrid(16);
+    } else if (x > 64) {
+        alert("Max value is 64. Sorry :(")
+        resizeGrid();
+    } else {
+        createGrid(x);
+    }
+}
+
+sizebutton.addEventListener('click', () => {
+    resizeGrid();
+});
